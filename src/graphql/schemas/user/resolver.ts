3d03ef0de3db7cc73b1fc45c2b5534766593560c
DavidMBP <steadypathapp@gmail.com>
2018-06-30 13:04:45 -0500

    Added resolvers to user

--- src/graphql/schemas/user/resolver.ts
+++ src/graphql/schemas/user/resolver.ts
@@ -1,12 +1,32 @@
+import {database} from 'database';
+
+
+/**
+*
+*/
+const friends = (obj, args, context) => {
+  return {
+    cursor: '',
+    friends:[]
+  }
+}
+
+
+/**
+*
+*/
+const notifications = (obj, args, context) => {
+  const {userId} = context
+
+  return database.notifictions.getForUser({
+    userId
+  })
+}
 
 
 export default {
   User: {
-    friends: (obj, args, context) => {
-
-    },
-    notifications: (obj, args, context) => {
-
-    }
+    friends,
+    notifications
   }
 }
