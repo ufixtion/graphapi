3add021ff39161d1e0211a0c5c6667928e18e499
DavidMBP <steadypathapp@gmail.com>
2018-07-01 20:05:08 -0500

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as cors from 'cors';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import { typeDefs, resolvers } from './graphql'
 import { makeExecutableSchema } from 'graphql-tools';
@@ -13,12 +14,12 @@ const schema = makeExecutableSchema({
 const app = express();
 
 // The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphql', cors(), bodyParser.json(), graphqlExpress({ schema }));
 
 // GraphiQL, a visual editor for queries
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
+app.listen(8000, () => {
+  console.log('Go to http://localhost:8000/graphiql to run queries!');
 });
